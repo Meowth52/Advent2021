fe44e722843dbb97067f425bcacc00aec5db1dba
Meowth52 <meowth@meowth.se>
2021-11-30 22:24:23 +0100

    Add project files.

---
+++ Day19.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Advent2021
+{
+    public class Day19 : Day
+    {
+        List<int> Instructions;
+        public Day19(string _input) : base(_input)
+        {
+            Instructions = this.parseListOfInteger(_input);
+        }
+        public override Tuple<string, string> getResult()
+        {
+            return Tuple.Create(getPartOne(), getPartTwo());
+        }
+        public string getPartOne()
+        {
+            int ReturnValue = 0;
+
+            return ReturnValue.ToString();
+        }
+        public string getPartTwo()
+        {
+            int ReturnValue = 0;
+
+            return ReturnValue.ToString();
+        }
+    }
+}
