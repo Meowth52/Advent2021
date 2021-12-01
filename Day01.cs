d33851c949eab47aeed3ed02ef11e61ce991218e
Meowth52 <meowth@meowth.se>
2021-12-01 08:13:16 +0100

    Day1

--- Day01.cs
+++ Day01.cs
@@ -12,6 +12,7 @@ namespace Advent2021
         List<int> Instructions;
         public Day01(string _input) : base(_input)
         {
+            string Input = this.CheckFile(_input);
             Instructions = this.parseListOfInteger(_input);
         }
         public override Tuple<string, string> getResult()
@@ -21,13 +22,30 @@ namespace Advent2021
         public string getPartOne()
         {
             int ReturnValue = 0;
+            int Previous = 1000000;
+            foreach (int i in Instructions)
+            {
+                if (i > Previous)
+                    ReturnValue++;
+                Previous = i;
+            }
 
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
-            int ReturnValue = 0;
-
+            int ReturnValue = -1;
+            int Previous = 0;
+            int Previouser = 1000000;
+            int Previousest = 1000000;
+            foreach (int i in Instructions)
+            {
+                if (i > Previousest)
+                    ReturnValue++;
+                Previousest = Previouser;
+                Previouser = Previous;
+                Previous = i;
+            }
             return ReturnValue.ToString();
         }
     }
