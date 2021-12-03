c04c624306a4e979be324541c97a65451f2b7928
Meowth52 <meowth@meowth.se>
2021-12-03 12:12:00 +0100

    Functional but..

--- Day03.cs
+++ Day03.cs
@@ -9,11 +9,22 @@ namespace Advent2021
 {
     public class Day03 : Day
     {
-        List<int> Instructions;
+        List<bool[]> Instructions;
         public Day03(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.parseListOfInteger(Input);
+            string[] halfway = this.parseStringArray(Input);
+            Instructions = new List<bool[]>();
+            foreach (string s in halfway)
+            {
+                List<bool> boolList = new List<bool>();
+                foreach (char c in s)
+                    if (c == '1')
+                        boolList.Add(true);
+                    else if (c == '0')
+                        boolList.Add(false);
+                Instructions.Add(boolList.ToArray());
+            }
         }
         public override Tuple<string, string> getResult()
         {
@@ -22,13 +33,101 @@ namespace Advent2021
         public string getPartOne()
         {
             int ReturnValue = 0;
-
+            int[] GammaCount = new int[Instructions[0].Count()];
+            bool[] Gamma = new bool[Instructions[0].Count()];
+            foreach (bool[] b in Instructions)
+            {
+                for (int i = 0; i < GammaCount.Count(); i++)
+                {
+                    if (b[i])
+                        GammaCount[i]++;
+                }
+            }
+            int GammaValue = 0;
+            int EpsilonValue = 0;
+            for (int i = 0; i < Gamma.Count(); i++)
+            {
+                if (GammaCount[i] > (Instructions.Count() / 2))
+                {
+                    Gamma[i] = true;
+                    GammaValue += (int)Math.Pow(2, (Instructions[0].Count() - 1) - i);
+                }
+                else
+                {
+                    EpsilonValue += (int)Math.Pow(2, (Instructions[0].Count() - 1) - i);
+                    Gamma[i] = false;
+                }
+            }
+            ReturnValue = GammaValue * EpsilonValue;
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
             int ReturnValue = 0;
+            int Oxygen = 0;
+            int Scrubb = 0;
+            List<bool[]> RemaingInstructions = new List<bool[]>(Instructions);
+            for (int i = 0; i < Instructions[0].Count(); i++)
+            {
+                int NumberOfTrue = 0;
+                bool Keep = false;
+                foreach (bool[] b in RemaingInstructions)
+                {
+                    if (b[i])
+                        NumberOfTrue++;
+                }
+                if (NumberOfTrue * 2 >= RemaingInstructions.Count)
+                {
+                    Keep = true;
+                }
+                List<bool[]> NextInstructions = new List<bool[]>();
+                foreach (bool[] b in RemaingInstructions)
+                {
+                    if (b[i] == Keep)
+                        NextInstructions.Add(b);
+                }
+                RemaingInstructions = new List<bool[]>(NextInstructions);
+                if (RemaingInstructions.Count == 1)
+                    break;
+            }
+            for (int i = 0; i < RemaingInstructions[0].Count(); i++)
+            {
+                if (RemaingInstructions[0][i])
+                    Oxygen += (int)Math.Pow(2, (Instructions[0].Count() - 1) - i);
+            }
+            RemaingInstructions = new List<bool[]>(Instructions);
 
+
+            // duplicate code should be method
+            for (int i = 0; i < Instructions[0].Count(); i++)
+            {
+                int NumberOfTrue = 0;
+                bool Keep = false;
+                foreach (bool[] b in RemaingInstructions)
+                {
+                    if (b[i])
+                        NumberOfTrue++;
+                }
+                if (NumberOfTrue * 2 < RemaingInstructions.Count)
+                {
+                    Keep = true;
+                }
+                List<bool[]> NextInstructions = new List<bool[]>();
+                foreach (bool[] b in RemaingInstructions)
+                {
+                    if (b[i] == Keep)
+                        NextInstructions.Add(b);
+                }
+                RemaingInstructions = new List<bool[]>(NextInstructions);
+                if (RemaingInstructions.Count == 1)
+                    break;
+            }
+            for (int i = 0; i < RemaingInstructions[0].Count(); i++)
+            {
+                if (RemaingInstructions[0][i])
+                    Scrubb += (int)Math.Pow(2, (Instructions[0].Count() - 1) - i);
+            }
+            ReturnValue = Oxygen * Scrubb;
             return ReturnValue.ToString();
         }
     }
