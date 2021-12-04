a9667307d65d3cd9a12df0fd0f357602eab62e8f
Meowth52 <meowth@meowth.se>
2021-12-04 11:16:57 +0100

    Just some notes

--- Day04.cs
+++ Day04.cs
@@ -9,11 +9,19 @@ namespace Advent2021
 {
     public class Day04 : Day
     {
-        List<int> Instructions;
+        List<List<int>> Instructions;
+        List<int> RandomNumbers;
+        List<int[,]> Boards;
         public Day04(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.parseListOfInteger(Input);
+            Instructions = this.parseListOfIntegerLists(Input);
+            RandomNumbers = new List<int>(Instructions[0]);
+            for (int i = 1; i < Instructions.Count(); i++)
+            {
+                if (i % 5 == 1)
+                    Boards.Add(new int[5, 5]); // or a dic?
+            }
         }
         public override Tuple<string, string> getResult()
         {
