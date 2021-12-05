7daef3bb67597da7bab83388ebd317d18de6b004
Meowth52 <meowth@meowth.se>
2021-12-05 23:07:40 +0100

    Del 1 ok 2 buggar

--- Day04.cs
+++ Day04.cs
@@ -11,17 +11,45 @@ namespace Advent2021
     {
         List<List<int>> Instructions;
         List<int> RandomNumbers;
-        List<int[,]> Boards;
+        Dictionary<int, List<Dictionary<int, bool>>> Boards;
+        int ReturnValue2;
         public Day04(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
             Instructions = this.parseListOfIntegerLists(Input);
             RandomNumbers = new List<int>(Instructions[0]);
-            for (int i = 1; i < Instructions.Count(); i++)
+            Instructions.RemoveAt(0);
+            Boards = new Dictionary<int, List<Dictionary<int, bool>>>();
+            List<Dictionary<int, bool>> Verticals = new List<Dictionary<int, bool>>();
+            int BoardIterator = 0;
+
+            for (int i = 0; i < Instructions.Count(); i++)
             {
-                if (i % 5 == 1)
-                    Boards.Add(new int[5, 5]); // or a dic?
+                if (i % 5 == 0)
+                {
+                    if (Verticals.Count == 5)
+                    {
+                        Boards[BoardIterator].AddRange(Verticals);
+                    }
+                    BoardIterator++;
+                    Boards.Add(BoardIterator, new List<Dictionary<int, bool>>());
+                    Verticals.Clear();
+                    for (int bi = 0; bi < 5; bi++)
+                        Verticals.Add(new Dictionary<int, bool>());
+                }
+                Dictionary<int, bool> Row = new Dictionary<int, bool>();
+                int ehh = 0;
+                foreach (int plupp in Instructions[i])
+                {
+                    Row.Add(plupp, false);
+                    Verticals[ehh].Add(plupp, false);
+                    ehh++;
+                }
+                Boards[BoardIterator].Add(Row);
+
             }
+
+            Boards[BoardIterator].AddRange(Verticals);
         }
         public override Tuple<string, string> getResult()
         {
@@ -30,12 +58,60 @@ namespace Advent2021
         public string getPartOne()
         {
             int ReturnValue = 0;
+            ReturnValue2 = 0;
+            List<int> BingoCount = new List<int>();
 
+            foreach (int r in RandomNumbers)
+            {
+                foreach (KeyValuePair<int, List<Dictionary<int, bool>>> Board in Boards)
+                {
+                    if (!BingoCount.Contains(Board.Key))
+                    {
+                        foreach (Dictionary<int, bool> row in Board.Value)
+                        {
+                            if (row.ContainsKey(r))
+                            {
+                                row[r] = true;
+
+                                if (!row.ContainsValue(false))
+                                {
+                                    if (ReturnValue == 0)
+                                    {
+                                        for (int i = 0; i < 5; i++)
+                                        {
+                                            foreach (KeyValuePair<int, bool> n in Board.Value[i])
+                                            {
+                                                if (!n.Value)
+                                                    ReturnValue += n.Key;
+                                            }
+                                        }
+                                        ReturnValue *= r;
+                                    }
+                                    BingoCount.Add(Board.Key);
+                                    if (BingoCount.Count == Boards.Count)
+                                    {
+                                        for (int i = 0; i < 5; i++)
+                                        {
+                                            foreach (KeyValuePair<int, bool> n in Board.Value[i])
+                                            {
+                                                if (!n.Value)
+                                                    ReturnValue2 += n.Key;
+                                            }
+                                        }
+                                        ReturnValue2 *= r;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
-            int ReturnValue = 0;
+            int ReturnValue = ReturnValue2;
 
             return ReturnValue.ToString();
         }
