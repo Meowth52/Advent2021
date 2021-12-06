d872f7dc9e25f150fdc152e933e655660f4c3b43
Meowth52 <meowth@meowth.se>
2021-12-06 10:00:04 +0100

    Day 6 done

--- Day06.cs
+++ Day06.cs
@@ -10,10 +10,18 @@ namespace Advent2021
     public class Day06 : Day
     {
         List<int> Instructions;
+        Dictionary<int, long> GroupedFish;
+        long ReturnValue2;
         public Day06(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
             Instructions = this.parseListOfInteger(Input);
+            GroupedFish = new Dictionary<int, long>();
+            ReturnValue2 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                GroupedFish.Add(i, Instructions.Count(x => x == i));
+            }
         }
         public override Tuple<string, string> getResult()
         {
@@ -21,13 +29,25 @@ namespace Advent2021
         }
         public string getPartOne()
         {
-            int ReturnValue = 0;
-
+            long ReturnValue = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                long Zeros = GroupedFish[0];
+                for (int schmi = 0; schmi < 9; schmi++)
+                {
+                    GroupedFish[schmi] = GroupedFish[schmi + 1];
+                }
+                GroupedFish[6] += Zeros;
+                GroupedFish[8] = Zeros;
+                if (i == 79)
+                    ReturnValue = GroupedFish.Values.Sum();
+            }
+            ReturnValue2 = GroupedFish.Values.Sum();
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
-            int ReturnValue = 0;
+            long ReturnValue = ReturnValue2;
 
             return ReturnValue.ToString();
         }
