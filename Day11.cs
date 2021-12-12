113c782fda2c5c5475cdf64a8eb03576b399e3d5
Meowth52 <meowth@meowth.se>
2021-12-12 11:04:33 +0100

    Beutify 11

--- Day11.cs
+++ Day11.cs
@@ -9,19 +9,21 @@ namespace Advent2021
 {
     public class Day11 : Day
     {
-        Dictionary<Coordinate,int> Octopussy;
+        Dictionary<Coordinate, int> Octopussy;
         List<Coordinate> OctoKeys;
+        int ReturnValue2;
         public Day11(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            string[]  rows = this.parseStringArray(Input);
-            Octopussy = new Dictionary<Coordinate, int>();;
-            for(int y = 0;y<rows.Length;y++)
-                for(int x= 0;x<rows[0].Length;x++)
-                        Octopussy.Add(new Coordinate(x,y),Int32.Parse(rows[y][x].ToString()));
+            string[] rows = this.parseStringArray(Input);
+            Octopussy = new Dictionary<Coordinate, int>(); ;
+            for (int y = 0; y < rows.Length; y++)
+                for (int x = 0; x < rows[0].Length; x++)
+                    Octopussy.Add(new Coordinate(x, y), Int32.Parse(rows[y][x].ToString()));
             OctoKeys = new List<Coordinate>();
-            foreach(KeyValuePair<Coordinate,int> c in Octopussy)
+            foreach (KeyValuePair<Coordinate, int> c in Octopussy)
                 OctoKeys.Add(c.Key);
+            ReturnValue2 = 0;
         }
         public override Tuple<string, string> getResult()
         {
@@ -30,31 +32,32 @@ namespace Advent2021
         public string getPartOne()
         {
             int ReturnValue = 0;
+            int TotalFlashes = 0;
             List<Coordinate> Flashes = new List<Coordinate>();
             List<Coordinate> Flashed = new List<Coordinate>();
             int i = 0;
-            while(true) 
-            { 
-                foreach(Coordinate c in OctoKeys) 
-                { 
+            while (true)
+            {
+                foreach (Coordinate c in OctoKeys)
+                {
                     Octopussy[c]++;
-                    if(Octopussy[c] > 9) 
-                    { 
+                    if (Octopussy[c] > 9)
+                    {
                         Flashes.Add(c);
-                        }
+                    }
                 }
-                Flashed=  new List<Coordinate>( Flashes);
-                while (Flashes.Count>0) 
+                Flashed = new List<Coordinate>(Flashes);
+                while (Flashes.Count > 0)
                 {
                     List<Coordinate> NextFlashes = new List<Coordinate>();
-                    foreach(Coordinate c in Flashes)
+                    foreach (Coordinate c in Flashes)
                     {
-                        foreach(Coordinate n in c.GetNeihbours(Diagonals:  true))
+                        foreach (Coordinate n in c.GetNeihbours(Diagonals: true))
                         {
-                            if (Octopussy.ContainsKey(n)) 
-                            { 
+                            if (Octopussy.ContainsKey(n))
+                            {
                                 Octopussy[n]++;
-                                if(Octopussy[n]>9&& !Flashed.Contains(n))
+                                if (Octopussy[n] > 9 && !Flashed.Contains(n))
                                 {
                                     NextFlashes.Add(n);
                                     Flashed.Add(n);
@@ -64,24 +67,28 @@ namespace Advent2021
                     }
                     Flashes = new List<Coordinate>(NextFlashes);
                 }
-                foreach(Coordinate c in OctoKeys)
-                    if (Octopussy[c] > 9) 
-                    { 
-                        Octopussy[c]=0;
-                        ReturnValue++;
-                        }
-                i++;
-                if (Flashed.Count == 100) 
-                { 
-                    ReturnValue = i;
-                    break;
+                foreach (Coordinate c in OctoKeys)
+                    if (Octopussy[c] > 9)
+                    {
+                        Octopussy[c] = 0;
+                        TotalFlashes++;
                     }
+                if (i == 99)
+                {
+                    ReturnValue = TotalFlashes;
+                }
+                i++;
+                if (Flashed.Count == 100)
+                {
+                    ReturnValue2 = i;
+                    break;
+                }
             }
             return ReturnValue.ToString();
         }
         public string getPartTwo()
         {
-            int ReturnValue = 0;
+            int ReturnValue = ReturnValue2;
 
             return ReturnValue.ToString();
         }
