f54e8e6888b58422ea46e7c3dbf552ce5d49e024
Meowth52 <meowth@meowth.se>
2021-12-13 18:21:50 +0100

    day 13 broken

--- Day13.cs
+++ Day13.cs
@@ -9,11 +9,24 @@ namespace Advent2021
 {
     public class Day13 : Day
     {
-        List<int> Instructions;
+        List<(char Axis, int Amount)> Instructions;
+        Dictionary<Coordinate, int> TheGrid;
         public Day13(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            Instructions = this.parseListOfInteger(Input);
+            string[] Splitted = this.parseStringArray(Input);
+            TheGrid = new Dictionary<Coordinate, int>();
+            Instructions = new List<(char Axis, int Amount)>();
+            foreach (string s in Splitted)
+            {
+                List<int> Ints = this.parseListOfInteger(s);
+                if (Ints.Count == 2)
+                    TheGrid.Add(new Coordinate(Ints[0], Ints[1]), 0);
+                else
+                {
+                    Instructions.Add((s[11], Ints[0]));
+                }
+            }
         }
         public override Tuple<string, string> getResult()
         {
@@ -22,7 +35,8 @@ namespace Advent2021
         public string getPartOne()
         {
             int ReturnValue = 0;
-
+            this.Fold(Instructions[0].Axis, Instructions[0].Amount);
+            ReturnValue = TheGrid.Count;
             return ReturnValue.ToString();
         }
         public string getPartTwo()
@@ -30,6 +44,34 @@ namespace Advent2021
             int ReturnValue = 0;
 
             return ReturnValue.ToString();
+        }
+        public void Fold(char axis, int amount)
+        {
+            Dictionary<Coordinate, int> NextGrid = new Dictionary<Coordinate, int>();
+            foreach (KeyValuePair<Coordinate, int> k in TheGrid)
+            {
+                Coordinate NewCoordinate = k.Key;
+                int Value = k.Value;
+                if (axis == 'x' && k.Key.x > amount)
+                {
+                    NewCoordinate = new Coordinate(k.Key.x - (amount + 1), k.Key.y);
+                    if (TheGrid.ContainsKey(NewCoordinate))
+                        Value++;
+                }
+                else if (k.Key.y > amount)
+                {
+                    NewCoordinate = new Coordinate(k.Key.x, k.Key.y - (amount + 1));
+                    if (TheGrid.ContainsKey(NewCoordinate))
+                        Value++;
+                }
+                if (!NextGrid.ContainsKey(NewCoordinate))
+                    NextGrid.Add(NewCoordinate, Value);
+                else
+                    NextGrid[NewCoordinate] = Math.Max(k.Value, Value);
+
+
+            }
+            TheGrid = new Dictionary<Coordinate, int>(NextGrid);
         }
     }
 }
