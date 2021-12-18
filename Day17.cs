2bf149940ec21d2ef90aa297d2e6375b3bac2270
Meowth52 <meowth@meowth.se>
2021-12-18 18:30:36 +0100

    part 1 done, not though

--- Day17.cs
+++ Day17.cs
@@ -11,12 +11,14 @@ namespace Advent2021
     {
         (int Min, int Max) XLimits;
         (int Min, int Max) YLimits;
+        int OwDamned;
         public Day17(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
             List<int> Instructions = this.ParseListOfInteger(Input);
             XLimits = (Instructions[0], Instructions[1]);
             YLimits = (Instructions[2], Instructions[3]);
+            OwDamned = 0;
         }
         public override Tuple<string, string> GetResult()
         {
@@ -25,11 +27,11 @@ namespace Advent2021
         public string GetPartOne()
         {
             int ReturnValue = 0;
-            for (int y = 100000; y > 0; y--)
+            for (int y = 10000; y > 0; y--)
             {
                 ReturnValue = HitOnY(YLimits, y);
                 if (ReturnValue > 0)
-                    if (HitOnX(XLimits, y))
+                    if (HitOnX(XLimits) > 0)
                     {
                         break;
                     }
@@ -39,44 +41,52 @@ namespace Advent2021
         public string GetPartTwo()
         {
             int ReturnValue = 0;
-
+            int CeheckValue = 0;
+            for (int y = 10000; y > (YLimits.Min - 1); y--)
+            {
+                CeheckValue = HitOnY(YLimits, y);
+                if (CeheckValue != 0)
+                {
+                    int well = HitOnX(XLimits);
+                    if (well > 0)
+                    {
+                        ReturnValue += well;
+                    }
+                }
+            }
             return ReturnValue.ToString();
         }
-        public bool HitOnX((int Max, int Min) Limits, int y)
+        public int HitOnX((int Min, int Max) Limits)
         {
-            bool Hit = false;
+            int Hit = 0;
             for (int i = 1; i <= Limits.Max; i++)
             {
                 int xv = i;
                 int x = 0;
-                Hit = false;
-                for (int xi = 0; xi <= y; xi++)
+                for (int xi = 0; xi <= OwDamned; xi++)
                 {
+                    if (xi == OwDamned && x >= Limits.Min && x <= Limits.Max)
+                    {
+                        Hit++;
+                    }
                     x += xv;
                     if (xv > 0)
                         xv--;
-                    else
-                        break;
-                    if (xi == y && x >= Limits.Min)
-                    {
-                        Hit = true;
-                        break;
-                    }
                 }
-                if (Hit)
-                    break;
             }
             return Hit;
         }
-        public int HitOnY((int Max, int Min) Limits, int Velocity)
+        public int HitOnY((int Min, int Max) Limits, int Velocity)
         {
-            int ReturnValue = 0;
+            int ReturnValue = 1;
             int yv = Velocity;
             bool Hit = false;
             int y = 0;
-            while (y >= Limits.Max)
+            OwDamned = 0;
+            while (y >= Limits.Min)
             {
-                if (y <= Limits.Min)
+                OwDamned++;
+                if (y <= Limits.Max)
                 {
                     Hit = true;
                     break;
