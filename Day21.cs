1c86ce1b0d0d882b637b7532a3717989d8e782c0
Meowth52 <meowth@meowth.se>
2021-12-23 21:00:56 +0100

    Day 21 part two failed attempt

--- Day21.cs
+++ Day21.cs
@@ -9,6 +9,7 @@ namespace Advent2021
 {
     public class Day21 : Day
     {
+        List<int> Instructions;
         int Player1;
         int Player2;
         int Score1;
@@ -17,7 +18,7 @@ namespace Advent2021
         public Day21(string _input) : base(_input)
         {
             string Input = this.CheckFile(_input);
-            List<int> Instructions = this.ParseListOfInteger(Input);
+            Instructions = this.ParseListOfInteger(Input);
             Player1 = Instructions[1];
             Player2 = Instructions[3];
             Score1 = 0;
@@ -56,9 +57,41 @@ namespace Advent2021
         }
         public string GetPartTwo()
         {
-            int ReturnValue = 0;
-
-            return ReturnValue.ToString();
+            int OPlayer1 = Instructions[1];
+            int OPlayer2 = Instructions[3];
+            long Player1Wins = 0;
+            long Player2Wins = 0;
+            int[] Dice = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            for (int i = 0; i < Dice.Length; i++)
+            {
+                for (int ii = 1; ii <= 3; ii++)
+                {
+                    Dice[i] = ii;
+                    Player1 = OPlayer1;
+                    Player2 = OPlayer2;
+                    Score1 = 0;
+                    Score2 = 0;
+                    for (int d = 0; d < Dice.Length; d += 6)
+                    {
+                        Player1 = DiracRound(Player1, ref Dice, d);
+                        Score1 += Player1;
+                        if (Score1 >= 21)
+                        {
+                            Player1Wins++;
+                            break;
+                        }
+                        Player2 = DiracRound(Player2, ref Dice, d + 3);
+                        Score2 += Player2;
+                        if (Score2 >= 21)
+                        {
+                            Player2Wins++;
+                            break;
+                        }
+                    }
+                    ;
+                }
+            }
+            return Math.Max(Player1Wins, Player2Wins).ToString();
         }
         public int Round(int player)
         {
@@ -67,6 +100,16 @@ namespace Advent2021
             Player = WieredModulus(Player, 10);
             Die += 3;
             Die = WieredModulus(Die, 100);
+            return Player;
+        }
+        public int DiracRound(int player, ref int[] Dice, int turn)
+        {
+            int Player = player;
+            for (int i = 0; i < 3; i++)
+            {
+                Player += Dice[turn + i];
+            }
+            Player = WieredModulus(Player, 10);
             return Player;
         }
         public int WieredModulus(int i, int m)
